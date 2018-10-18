2c0d3cd8469ce781446848de95a900795f2ee2b3
IlonaKolo <ilona.kolossova@khk.ee>
2018-10-18 14:50:14 +0300

    creating api_controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,10 @@
+class ApiController < ApplicationController
+		 skip_before_action :authenticate_user!
+  def about
+
+  		render json: 
+  		{Name: 'Ilona', Date: Time.now.strftime("%d/%m/%Y"),
+  		 Time: Time.now.strftime("%H:%M"), Fruits: 'Apple, Banana'}  		
+  		
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'IlonaKolo',
+  :password => ENV['SENDGRID_API_KEY'],
+  :domain => 'ilona.vso17.cariba.ee',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+
