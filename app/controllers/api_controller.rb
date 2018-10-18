0e08faff543eaccd71053ec44b9d09471220ad44
IlonaKolo <ilona.kolossova@khk.ee>
2018-10-18 16:46:26 +0300

    haiku reverse

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,25 @@
 class ApiController < ApplicationController
 		 skip_before_action :authenticate_user!
+		 skip_before_action :verify_authenticity_token
   def about
 
-  		render json: 
-  		{name: 'Ilona', date: Time.now.strftime("%d-%m-%Y"),
-  		 time: Time.now.strftime("%H:%M"), fruits: ['Apple, Banana, Pineapple']}  		
+  	render json: 
+  	{name: 'Ilona', date: Time.now.strftime("%d-%m-%Y"),
+  	 time: Time.now.strftime("%H:%M"), fruits: ['Apple, Banana, Pineapple']}  		
   		
   end
+
+
+  def haiku
+  	render json: { haiku: reverse(haiku_params[:haiku]), name: haiku_params[:name].upcase}
+  end 
+
+  def haiku_params
+  	params.permit(:name, :haiku)
+  end
+
+  def reverse(haiku)
+  	haiku.split(' ').reverse.join(' ')
+  end 
+
 end
